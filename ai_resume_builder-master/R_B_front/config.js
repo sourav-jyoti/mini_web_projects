555116860dbfb545bf3896172b30e78b90de3837
sourav-jyoti <souravjyoti64@gmail.com>
2025-07-13 18:14:29 +0530

    minor change

--- ai_resume_builder-master/R_B_front/config.js
+++
@@ -1,7 +0,0 @@
-const REACT_APP_API_URL = 'https://ai-resume-builder-back-bewo.onrender.com'; // Backend Render URL
-//no need to write NODE_ENV as it is set by render as production
-
-export const API_URL =
-  process.env.NODE_ENV === 'production'
-    ? REACT_APP_API_URL
-    : 'http://localhost:3000';
--- ai_resume_builder-master/R_B_front/context/ResumeContext.jsx
+++
@@ -1,4 +0,0 @@
-import {createContext} from 'react';
-
-
-export const ResumeContext = createContext(null);
